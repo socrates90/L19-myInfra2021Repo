3473fac240948f689d880e92cd9772143fe33fc7
Socrates Lopez <socrates.lopez@outlook.com>
2022-09-17 19:46:30 -0500

    fixed bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "lab 19-tf-state-bucket"
+    bucket = "lab19-tf-state-bucket"
     key = "lockID"
     region = "us-east-1"
     dynamodb_table = "L19-DB-Table"
