d2cdae0c42b7edb4c25b01c787a939b203d51a9f
socrates90 <socrates.lopez@outlook.com>
2024-03-22 22:42:35 +0000

    Added missing s3bucket resource, only versioning listed before

--- s3.tf
+++ s3.tf
@@ -1,3 +1,11 @@
+resource "aws_s3_bucket" "my-s3-bucket" {
+  bucket_prefix = var.bucket_prefix
+  acl           = var.acl
+
+  tags = var.tags
+}
+
+
 resource "aws_s3_bucket_versioning" "my-s3-bucket-versioning" {
   bucket = aws_s3_bucket.my-s3-bucket.id
   versioning_configuration {
